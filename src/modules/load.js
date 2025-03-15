ae4a7f404f6bb85016c917bbcd331a8c693985ce
Gerasimovich Sergey <wmmoee@gmail.com>
2025-03-15 15:55:18 +0300

    cart render

--- src/modules/load.js
+++ src/modules/load.js
@@ -8,4 +8,5 @@ const load = () => {
     });
 };
 
+
 export default load;
--- src/modules/priceRangeFilter.js
+++ src/modules/priceRangeFilter.js
@@ -5,27 +5,28 @@ import { priceFilter, saleFilter } from './filters';
 
 const priceRangeFilter = () => {
     const filter = document.querySelector('.filter form');
+    const minPrice = filter.querySelector('#min');
+    const maxPrice = filter.querySelector('#max');
     const checkboxInput = document.querySelector('#discount-checkbox');
     const checkboxSpan = document.querySelector('.filter-check_checkmark');
     
     filter.addEventListener('input', () => {
-        let minPrice = filter.querySelector('#min').value;
-        let maxPrice = filter.querySelector('#max').value;
-        
         getData().then((data) => {
-            renderGoods(priceFilter(data, minPrice, maxPrice));
+            renderGoods(priceFilter(saleFilter(data, checkboxInput.checked), minPrice.value, maxPrice.value));
         });
     });
     
     checkboxInput.addEventListener('change', () => {
-        if (checkboxInput.checked) {
+        const checked = checkboxInput.checked;
+        
+        if (checked) {
             checkboxSpan.classList.add('checked');
         } else {
             checkboxSpan.classList.remove('checked');
         }
         
         getData().then((data) => {
-            renderGoods(saleFilter(data, checkboxInput.checked));
+            renderGoods(priceFilter(saleFilter(data, checked), minPrice.value, maxPrice.value));
         });
     });
 };
